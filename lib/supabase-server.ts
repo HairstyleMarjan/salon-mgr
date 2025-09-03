36b846431d21d5fc46c98255f7d94603c5f81683
HairstyleMarjan <voolstradaan@gmail.com>
2025-09-03 16:08:24 +0200

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export const createServer = () => {
+export const createServerClient = () => {
   const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
